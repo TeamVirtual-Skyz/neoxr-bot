d63866f0c0badaacdab214877ec18360399fe734
TeamVirtual-Skyz <91053310+TeamVirtual-Skyz@users.noreply.github.com>
2022-09-19 01:01:50 +0700

    Update me.js

--- plugins/userinfo/me.js
+++ plugins/userinfo/me.js
@@ -15,12 +15,12 @@ exports.run = {
          let now = new Date() * 1
          let lastseen = (user.lastseen == 0) ? 'Never' : Func.toDate(now - user.lastseen)
          let usebot = (user.usebot == 0) ? 'Never' : Func.toDate(now - user.usebot)
-         let caption = `▬❏ *U S E R - P R O F I L E ❏▬*\n\n`
+         let caption = `❏ *U S E R - P R O F I L E ❏*\n\n`
          caption += `	◦ *Name* : ${m.pushName}\n`
          caption += `	◦ *Limit* : ${Func.formatNumber(user.limit)}\n`
          caption += `	◦ *Hitstat* : ${Func.formatNumber(user.hit)}\n`
          caption += `	◦ *Warning* : ${((m.isGroup) ? (typeof global.db.groups[m.chat].member[m.sender] != 'undefined' ? global.db.groups[m.chat].member[m.sender].warning : 0) + ' / 5' : user.warning + ' / 5')}\n\n`
-         caption += `▬❏ *U S E R - S T A T U S* ❏▬\n\n`
+         caption += `❏ *U S E R - S T A T U S* ❏\n\n`
          caption += `	◦ *Blocked* : ${(blocked ? '√' : '×')}\n`
          caption += `	◦ *Banned* : ${(new Date - user.banTemp < global.timer) ? Func.toTime(new Date(user.banTemp + global.timer) - new Date()) + ' (' + ((global.timer / 1000) / 60) + ' min)' : user.banned ? '√' : '×'}\n`
          caption += `	◦ *Use In Private* : ${(Object.keys(global.db.chats).includes(m.sender) ? '√' : '×')}\n`
