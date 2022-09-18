6aff3a4ce5fd0c31bba859bf4e01223a677d6b45
TeamVirtual-Skyz <91053310+TeamVirtual-Skyz@users.noreply.github.com>
2022-09-18 21:02:34 +0700

    Update plugin.js

--- plugins/owner/plugin.js
+++ plugins/owner/plugin.js
@@ -9,17 +9,17 @@ exports.run = {
 	  let pluginDisable = global.db.setting.pluginDisable
       if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'tiktok'), m)
       let plugins = Object.keys(global.client.plugins)
-      if (!plugins.includes(args[0])) return client.reply(m.chat, Func.texted('bold', `🚩 Plugin ${args[0]}.js not found.`), m)
+      if (!plugins.includes(args[0])) return client.reply(m.chat, Func.texted('bold', `🧸 Plugin ${args[0]}.js not found.`), m)
       if (command == 'plugdis') {
-         if (pluginDisable.includes(args[0])) return client.reply(m.chat, Func.texted('bold', `🚩 Plugin ${args[0]}.js previously has been disabled.`), m)
+         if (pluginDisable.includes(args[0])) return client.reply(m.chat, Func.texted('bold', `🧸 Plugin ${args[0]}.js previously has been disabled.`), m)
          pluginDisable.push(args[0])
-         client.reply(m.chat, Func.texted('bold', `🚩 Plugin ${args[0]}.js successfully disabled.`), m)
+         client.reply(m.chat, Func.texted('bold', `🧸 Plugin ${args[0]}.js successfully disabled.`), m)
       } else if (command == 'plugen') {
-         if (!pluginDisable.includes(args[0])) return client.reply(m.chat, Func.texted('bold', `🚩 Plugin ${args[0]}.js not found.`), m)
+         if (!pluginDisable.includes(args[0])) return client.reply(m.chat, Func.texted('bold', `🧸 Plugin ${args[0]}.js not found.`), m)
          pluginDisable.forEach((data, index) => {
             if (data === args[0]) pluginDisable.splice(index, 1)
          })
-         client.reply(m.chat, Func.texted('bold', `🚩 Plugin ${args[0]}.js successfully enable.`), m)
+         client.reply(m.chat, Func.texted('bold', `🧸 Plugin ${args[0]}.js successfully enable.`), m)
       }
    },
    owner: true
