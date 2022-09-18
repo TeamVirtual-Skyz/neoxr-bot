816689e40c7067240e98c1c0c8fb86bcd9e8d179
TeamVirtual-Skyz <91053310+TeamVirtual-Skyz@users.noreply.github.com>
2022-09-18 21:11:29 +0700

    Update filter.js

--- plugins/event/group/filter.js
+++ plugins/event/group/filter.js
@@ -14,12 +14,12 @@ exports.run = {
             if (body && (new RegExp('\\b' + toxic.join('\\b|\\b') + '\\b')).test(body.toLowerCase())) {
                groupSet.member[m.sender].warning += 1
                let warning = groupSet.member[m.sender].warning
-               if (warning > 4) return client.reply(m.chat, Func.texted('bold', `🚩 Warning : [ 5 / 5 ], good bye ~~`), m).then(() => {
+               if (warning > 4) return client.reply(m.chat, Func.texted('bold', `🧸 Warning : [ 5 / 5 ], good bye ~~`), m).then(() => {
                   client.groupParticipantsUpdate(m.chat, [m.sender], 'remove').then(async () => {
                      groupSet.member[m.sender].warning = 0
                   })
                })
-               return client.reply(m.chat, `乂  *W A R N I N G* \n\nYou got warning : [ ${warning} / 5 ]\n\If you get 5 warnings you will be kicked automatically from the group.`, m)
+               return client.reply(m.chat, `🔥 *W A R N I N G* 🔥 \n\n Peringatan..!! : [ ${warning} / 5 ]\n\Jika Anda mendapatkan 5 peringatan, Anda akan ditendang secara otomatis dari grup.`, m)
             }
          }
       } catch (e) {
